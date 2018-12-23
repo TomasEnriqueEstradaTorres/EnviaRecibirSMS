e40bc3bc55de002e152eabd304445e5979e61236
Tomas Enrique <tomasenriquea80@gmail.com>
2018-12-23 16:20:34 +0100

    Interfaz grafica
    Se ha creado la interfaz grafica de la aplicacion y un metodo 'setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);' que sevira para justificar en este caso el texto del mensaje a enviar

--- app/src/main/java/com/example/yeialel/enviarecibirsms/MainActivity.java
+++ app/src/main/java/com/example/yeialel/enviarecibirsms/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.yeialel.enviarecibirsms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText numeroTelefono, mensajeEnviar;
+    Button enviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numeroTelefono = (EditText) findViewById(R.id.editTextNumeroTelefono);
+        mensajeEnviar = (EditText) findViewById(R.id.editTextMensajeTexto);
+        enviar = (Button) findViewById(R.id.buttonEnviar);
+
+
+        /** Esto de aqui sirve para justificar el texto en donde:
+         * @param JUSTIFICATION_MODE_INTER_WORD devuelve un valor entero = 1
+         *
+         * El otro valor y es por defecto es 'JUSTIFICATION_MODE_NONE' = 0   */
+        mensajeEnviar.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
+
+
+
+
     }
 }
